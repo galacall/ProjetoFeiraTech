84f1c576c86994f1b6c9344ef1988eea44c69d19
DiegoFerreira <diegofs2308@gmail.com>
2023-10-03 16:11:40 -0300

    Adicionei o backend no projeto com Slim e Monolog.

---
+++ src/main.php
@@ -0,0 +1,33 @@
+<?php
+
+include_once __DIR__ . "/../vendor/autoload.php";
+
+use App\SystemServices\MonologFactory;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Selective\BasePath\BasePathMiddleware;
+use Slim\Factory\AppFactory;
+
+//use App\SystemServices\SlimFactory;
+
+
+MonologFactory::getInstance()->debug("main executando...");
+
+$app = AppFactory::create();
+
+$app->addRoutingMiddleware();
+$app->add(new BasePathMiddleware($app));
+$app->addErrorMiddleware(true, true, true);
+
+$app->get('/', function (Request $request, Response $response) {
+   $response->getBody()->write('Olá Mundo!');
+   return $response;
+});
+
+$app->get('/inserirusuario', function (Request $request, Response $response) {
+    //executar a logica da sua aplicacao
+    $response->getBody()->write('Usuário Inserido!');
+    return $response;
+ });
+ 
+$app->run();
