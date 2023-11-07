39317f02fc9e75ac2421d13943bb495e391559ff
DiegoFerreira <diegofs2308@gmail.com>
2023-11-07 17:14:00 -0300

    Adicionei header

--- src/conexao.php
+++ src/conexao.php
@@ -1,5 +1,11 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Credentials: true');
+header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header("Content-Type: application/json; charset=UTF-8");
+
 $hostname = "localhost"; //Endereço do Banco, pode ser um link 
 $bancodedados = "feiratecno"; //
 $usuario = "root";
