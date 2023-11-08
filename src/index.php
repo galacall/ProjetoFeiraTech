801e28b03d1d1d5809c8f2d5333b8663ca086edb
Sora018 <renan.mc.2005.2014@gmail.com>
2023-11-07 22:16:13 -0300

    base do funcionamento do upload

--- src/index.php
+++ src/index.php
@@ -63,3 +63,36 @@
         <input type="file">
         <button type="submit">Enviar arquivo</button>
     </form>
+
+
+
+
+
+<?php>
+echo pathinfo("taphole", PATHINFO_EXTENSION);
+
+    if(isset($_FILES['arquivo'])) {
+        $arquivo = $_FILES['arquivo'];
+
+        if($arquivo['error'])
+        die("Falha ao enviar arquivo");
+
+        if($arquivo['size'] > 2097152)
+        die("Arquivo muito grande!! Max: 2MB");
+
+        $pasta = "arquivos/";
+        $nomeDoArquivo = $arquivo['name'];
+        $novoNomeDoArquivo = uniqid();
+        $extensao = strtolower(pathinfo($nomeDoArquivo, PATHINFO_EXTENSION));
+
+        if($extensao != "jpg" && $extensao != 'png')
+        die("Tipo de arquivo não aceito");
+
+        $deu_certo = move_uploaded_file($arquivo["tmp_name"], $path);
+        if ($deu_certo)
+        $mysqli->query("INSERT INTO arquivos (nome, path) VALUES ('', ''") or die($mysqli->error);
+            //echo "<p>Arquivo enviado com sucesso! Para acessá-lo, <a target=\"_blank\" href=\"arquivos/$novoNomeDoArquivo.$extensao/">Taphole</a></p>;
+        else
+         echo "<p>Falha ao enviar arquivo</p>";
+    }
+?>
