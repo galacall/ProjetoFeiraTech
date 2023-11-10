9172137d9457f645b65e4873cbfc5fd96f5181bb
W4rp-Pedro <pedoro.a16@gmail.com>
2023-11-09 21:49:54 -0300

    Adcionei sistema de comentarios

--- src/index.php
+++ src/index.php
@@ -1,60 +1,45 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Projeto BackEnd</title>
-</head>
-<body>
-    <h1>Projeto Php Com Monolog e Slim</h1>
-    <?php
-    include_once("conexao.php");
-    if(isset($_POST['email']) or isset($_POST['senha'])) {
-        if(strlen($_POST['email']) == 0) {
-            echo "Preenche o seu email.";
-        } else if(strlen($_POST['senha']) == 0) {
-            echo "Preencha a sua senha";
-        } else {
-            $email = $mysqli->real_escape_string($_POST['email']);
-            $senha = $mysqli->real_escape_string($_POST['senha']);
+<form action="" method="post">
+    <p>Nome de Usuário<input type="text" name="txtusu"></p>
+    <p>Comentário<input type="text" name="txtcom"></p>
+    <p><input type="submit" name="btn" value="Enviar"></p>
+</form>
 
-            $sql_code = "SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'";
-            $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL" . mysqli->error);
+<?php
+ require_once("conecta.php"); // Ajuste o caminho conforme a estrutura real do seu projeto
 
-            $quantidade = $sql_query->num_rows;
+ $conexao = con(); // Armazena a conexão em uma variável
 
-            if($quantidade == 1) {
+ 
+ if (isset($_POST['btn'])) {
+   
+    $id_comentario = $_POST['txtusu'];
+    $comentario = $_POST['txtcom'];
+    $sql = "
+        INSERT INTO tb_comentarios
+        (id_comentario, comentario)
+        VALUES ('$id_comentario', '$comentario')
+    ";
 
-                $usuario = $sql_query->fetch_assoc();
-                if(!isset($_SESSION)) {
-                    session_start();
-                }
+    $res = mysqli_query($conexao, $sql); // Executa a consulta usando a conexão armazenada
+}
 
-                $_SESSION['id'] = $usuario['id'];
-                $_SESSION['username'] = $usuario['username'];
-                //Sessão continua válida mesmo quando ele troca de página
-                //Post = enviado por um formulário
-                header("Location: teste.php");
-                //Redirecionando o usuário
-
-            } else {
-                "Falha ao logar! E-mail ou senha incorretos";
-            }
-
-        }
+    $sql2 = "
+        SELECT * FROM tb_comentarios
+        ORDER BY datahora DESC
+    ";
+    $res2 = mysqli_query($conexao, $sql2);
+    echo "
+        <table border='1'>
+            <tr>
+            <th>ID#</th><th>Comentário</th><th>Data Hora</th>
+            </tr>
+    ";
+    while($linha = mysqli_fetch_array($res2, 2)) {
+        echo "
+            <tr>
+            <td>$linha[0]</td><td>$linha[1]</td><td>$linha[2]</td>
+            </tr>
+        ";
     }
-    ?>
-    <form action="" method="POST">
-        <p>
-            <label>E-mail</label>
-            <input type="text" name="email">
-        </p><p>    
-            <label>Senha</label>
-            <input type="password" name="senha">
-        </p><p>
-            <button type="submit">Login</button>
-        </p>
-    </form>
-</body>
-</html>
+    echo "</table>"
+?>
